56e4f689401c6a0d417d57030a2670d882229144
shuvo794 <shuvohossain794@gmail.com>
2023-12-17 09:01:13 +0600

    change

---
+++ script.js
@@ -0,0 +1,62 @@
+// document.title='Dom Learn with boss '
+
+// Get Element Id 
+
+// let headerElements=document.getElementById('header');
+// // headerElements.innerText='change';
+// // headerElements.textContent='To_Apps'
+
+// headerElements.style.color='green';
+// headerElements.style.fontSize='50px'
+
+// let items=document.getElementsByClassName('item')
+
+// for(let i=0; i<=items.length;i++){
+//     items[i].style.color='red';
+// }
+
+// Get Element Tag
+
+// let test=document.getElementsByTagName('h1');
+// console.log(test);
+
+
+// Get quarry slector.
+
+// let checker=document.querySelector('h1');
+
+// console.log(checker.style.color='red');
+
+
+// const parent=document.querySelector('#items');
+
+// const children=parent.children;
+
+// console.log(children[2]);
+
+
+// const divElement=document.createElement('div')
+// divElement.className='red';
+// divElement.setAttribute('id','red');
+// divElement.setAttribute('tittle','new div');
+
+// const container=document.querySelector('.todo-list')
+// const h2Element=container.querySelector('h2');
+
+// container.insertBefore(divElement,h2Element);
+
+// Event lisenr
+
+// const headerElements=document.querySelector('#header');
+
+// headerElements.addEventListener('click',(event)=>{
+//     console.log(event);
+// });
+
+
+
+
+
+
+
+
